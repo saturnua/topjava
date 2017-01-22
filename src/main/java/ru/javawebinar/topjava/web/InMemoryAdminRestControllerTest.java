96395d2c6288eac961ab978f02fdd05ca797f1a7
saturnua <saturnua@gmail.com>
2017-01-22 18:21:18 +0200

    3_8 patch apply

--- src/main/java/ru/javawebinar/topjava/web/InMemoryAdminRestControllerTest.java
+++ src/main/java/ru/javawebinar/topjava/web/InMemoryAdminRestControllerTest.java
@@ -1,8 +1,12 @@
 package ru.javawebinar.topjava.web;
 
-import org.junit.*;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import ru.javawebinar.topjava.UserTestData;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
